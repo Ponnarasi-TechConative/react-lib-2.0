58317974d243ec5870bffcdf485b0114682c6769
Ponnarasi <ponnarasi.a@techconative.com>
2023-07-31 20:44:34 +0530

    initial commit

---
+++ dev-helpers/dev-helper-initializer.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+window.onload = function() {
+  window["RestImportBundle"] = window["rest-import-ui-bundle"]
+  window["RestImportStandalonePreset"] = window["rest-import-ui-standalone-preset"]
+  // Build a system
+  const ui = RestImportBundle({
+    dom_id: "#rest-import-ui",
+    url : 'https://petstore.swagger.io/v2/swagger.json',
+    value:{
+      id:"1",
+      "name":'Ponnarasi'
+    },
+    presets: [
+      RestImportBundle.presets.apis,
+      RestImportStandalonePreset
+    ],
+    plugins: [
+      RestImportBundle.plugins.DownloadUrl
+    ],
+    // requestSnippetsEnabled: true,
+    layout: "StandaloneLayout"
+  })
+
+  window.ui = ui
+
+  // ui.initOAuth({
+  //   clientId: "your-client-id",
+  //   clientSecret: "your-client-secret-if-required",
+  //   realm: "your-realms",
+  //   appName: "your-app-name",
+  //   scopeSeparator: " ",
+  //   scopes: "openid profile email phone address",
+  //   additionalQueryStringParams: {},
+  //   useBasicAuthenticationWithAccessCodeGrant: false,
+  //   usePkceWithAuthorizationCodeGrant: false
+  // })
+}
